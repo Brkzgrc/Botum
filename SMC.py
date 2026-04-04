0f6b8467e367101de7e3174b1e8c69c1af9b0c12
Brkzgrc <burakozgurcu@gmail.com>
2026-04-05 01:11:05 +0300

    Update SMC.py

--- SMC.py
+++ SMC.py
@@ -5,6 +5,19 @@ import time
 import json
 import os
 
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "SMC Sniper v3 is Running!", 200
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+    
 # ============================================================
 # 1) AYARLAR
 # ============================================================
